f4964af7f8bf5226e954c965ae63fc3b5bd3b803
berkayermis <mberkayermis@gmail.com>
2021-08-07 02:31:29 +0300

    deploying and token configured

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
+
 
   //add minter changed event
+  //basically it tracks the logs everytime that minter changed
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("Medipol Coin", "MCO") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender == minter , 'Error');
+    minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error');
 		_mint(account, amount);
 	}
 }
